2590179c8608077700ffca67924f064504374507
JanisPaplavskis <124462743+JanisPaplavskis@users.noreply.github.com>
2023-02-06 19:55:25 +0200

    Create Point.cs

---
+++ Point.cs
@@ -0,0 +1,9 @@
+namespace MySuperBank
+{
+    public class Point
+    {
+        public int X { get; }
+        public int Y { get; }
+        public Point(int x, int y) => (X, Y) = (x, y);
+    }
+}
